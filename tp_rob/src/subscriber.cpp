3a596bef4838078b4c6feb81ca4bfa6a62626026
seifEddy <40058486+seifEddy@users.noreply.github.com>
2022-10-04 05:54:20 -0400

    Add files via upload

--- tp_rob/src/subscriber.cpp
+++ tp_rob/src/subscriber.cpp
@@ -9,11 +9,11 @@ void positionCallback(const std_msgs::Float32::ConstPtr& msg)
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "listener");
+  ros::init(argc, argv, "subscriber");
 
   ros::NodeHandle n;
 
-  ros::Subscriber sub = n.subscribe("chatter", 1000, positionCallback);
+  ros::Subscriber sub = n.subscribe("position", 1000, positionCallback);
 
   ros::spin();
 
